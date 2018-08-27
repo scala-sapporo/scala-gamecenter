1d3057a4455fe935a14790ba4363437d30ca97ae
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2018-08-28 06:02:32 +0900

    Room構想

--- server/src/main/scala/org/scalasapporo/gamecenter/room/Room.scala
+++ server/src/main/scala/org/scalasapporo/gamecenter/room/Room.scala
@@ -1,10 +1,18 @@
 package org.scalasapporo.gamecenter.room
 
-case class Room private(members: Seq[Member])
+case class Room private(
+  roomName: String,
+  gameType: GameType,
+  members: Seq[Member]
+)
 object Room {
-    def apply(members: Seq[Member]): Room = new Room(members)
+  def apply(roomName: String, gameType: GameType, members: Seq[Member]): Room =
+    new Room(roomName, gameType, members)
 }
 
 sealed trait Member
 case class Owner() extends Member
 case class Guest() extends Member
+
+sealed trait GameType
+case object GameTypeTetrics extends GameType
